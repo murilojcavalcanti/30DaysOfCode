1597d146bf6b269bcb9e21725e2ea550105abb9d
murilojcavalcanti <josemurilo7@gmail.com>
2024-10-28 19:54:30 -0300

    Remove duplicadas 2

---
+++ RemoverduplicatasArrayII/Program.cs
@@ -0,0 +1,42 @@
+﻿
+int RemoveDuplicates(int[] nums)
+{
+    int count = 0;
+    Dictionary<int, int> numsRep = new Dictionary<int, int>();
+    
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (numsRep.ContainsKey(nums[i]) && numsRep[nums[i]] == 0)
+        {
+            numsRep[nums[i]]=1;
+            continue;
+        }
+        if (!numsRep.ContainsKey(nums[i]))
+        {
+            numsRep[nums[i]] = 0;
+        }
+    }
+
+    foreach (int i in numsRep.Keys)
+    {
+        int iterator = 0;
+        while (iterator <= numsRep[i] )
+        {
+            
+                nums[count] = i;
+                count++;
+                iterator++;
+        }
+
+    }
+    return count;
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        Console.WriteLine(nums[i]);
+    }
+    Console.WriteLine();
+    Console.WriteLine(count);
+}
+
+RemoveDuplicates([0, 0, 1, 1, 1, 1, 2, 3, 3]);

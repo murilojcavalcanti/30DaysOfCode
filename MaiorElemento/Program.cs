900d919120a58c630bc06c4b50c04a5176840934
murilojcavalcanti <josemurilo7@gmail.com>
2024-10-28 19:54:44 -0300

    Elemento que mais se repete

---
+++ MaiorElemento/Program.cs
@@ -0,0 +1,30 @@
+﻿int MajorityElement(int[] nums)
+{
+    
+    int n = 0;
+    Dictionary<int,int> numsRep = new Dictionary<int,int>();
+
+    for (int i =0;i<nums.Length;i++)
+    {
+        if (numsRep.ContainsKey(nums[i]))
+        {
+            numsRep[nums[i]]++;
+        }
+        else
+        {
+            numsRep.Add(nums[i], 1);
+        }
+
+        if (i==0)
+        {
+            n = nums[i];
+        }else  if (numsRep[nums[i]] > numsRep[n])
+        {
+            n = nums[i];
+        }
+    }
+    return n;
+
+}
+
+Console.WriteLine(MajorityElement([6,5,5]));

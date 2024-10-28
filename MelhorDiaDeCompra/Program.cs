024409308dda459ca211ba1e2576ec99e0504d9f
murilojcavalcanti <josemurilo7@gmail.com>
2024-10-28 19:54:54 -0300

    Melhor dia de compra

---
+++ MelhorDiaDeCompra/Program.cs
@@ -0,0 +1,23 @@
+﻿ int MaxProfit(int[] prices)
+{
+	int diferenca = 0;
+	int min = prices[0];
+	for (int i = 1; i < prices.Length; i++)
+	{
+		if (prices[i] < min)
+		{
+			min = prices[i];
+		}
+		
+		if ((prices[i]-min) > diferenca)
+		{
+            diferenca = prices[i]-min;
+		}
+	}
+
+
+	return diferenca;
+}
+
+
+MaxProfit([1,2]);

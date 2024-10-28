0e83cb0e050b2b9d36608dd7f8f1cada18c9a73d
murilojcavalcanti <josemurilo7@gmail.com>
2024-10-28 19:54:20 -0300

    Remove duplicadas

---
+++ RemoveElementoDuplicado/Program.cs
@@ -0,0 +1,25 @@
+﻿int RemoveDuplicates(int[]nums)
+{
+    int count = 1;
+    for (int i = 0; i <nums.Length-1; i++)
+    {
+        if (nums[i] == nums[i + 1])
+            continue;
+
+        nums[count] = nums[i+1];
+        count++;
+    }
+
+    foreach (int i in nums)
+    {
+        Console.WriteLine(i);
+
+    }
+    Console.WriteLine($"elementos:{count}");
+
+    return count;
+
+}
+
+RemoveDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]);
+
